4ccaf2e4a7b583e27a22e406d7f9065bec74b8e1
Dániel Boros <daniel.boros.17@gmail.com>
2023-08-07 18:49:31 +0200

    Add prettier

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    plugins: ["prettier-plugin-tailwindcss"],
+    tailwindConfig: "./tailwind.config.js",
+    trailingComma: "es5",
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+};
