1c44e8eccef58f82b71ed44c82612270251dcb60
Dániel Boros <daniel.boros.17@gmail.com>
2023-08-08 19:19:03 +0200

    Add radar and sidebar components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RootComponent } from './root/root.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { RadarComponent } from './radar/radar.component';
+import { LocationComponent } from './location/location.component';
 
 @NgModule({
     declarations: [RootComponent, SidebarComponent, RadarComponent],
-    imports: [BrowserModule],
+        RootComponent,
+        SidebarComponent,
+        RadarComponent,
     providers: [],
     bootstrap: [RootComponent],
 })
---
+++ src/app/radar/radar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'radar',
+    templateUrl: './radar.component.html',
+    styleUrls: ['./radar.component.css'],
+})
+export class RadarComponent {}
