ae36dd1ad2008ca297b3a6c6b6503721a909c283
Dániel Boros <daniel.boros.17@gmail.com>
2023-08-10 14:09:22 +0200

    Add forecast and metadata components

---
+++ src/app/metadata/metadata.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'metadata',
+    templateUrl: './metadata.component.html',
+    styleUrls: ['./metadata.component.css'],
+})
+export class MetadataComponent {}
