4405b0622f865d08093ae24c9a0d98a4bd967130
Dániel Boros <daniel.boros.17@gmail.com>
2023-08-10 14:27:02 +0200

    Move components to a separate directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RootComponent } from './root/root.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { RadarComponent } from './radar/radar.component';
-import { LocationComponent } from './location/location.component';
-import { ConditionsComponent } from './conditions/conditions.component';
+import { RootComponent } from './components/root/root.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { RadarComponent } from './components/radar/radar.component';
+import { LocationComponent } from './components/location/location.component';
+import { ConditionsComponent } from './components/conditions/conditions.component';
 import { NgOptimizedImage } from '@angular/common';
-import { ForecastComponent } from './forecast/forecast.component';
-import { AlertsComponent } from './alerts/alerts.component';
-import { RainfallComponent } from './rainfall/rainfall.component';
-import { MetadataComponent } from './metadata/metadata.component';
+import { ForecastComponent } from './components/forecast/forecast.component';
+import { AlertsComponent } from './components/alerts/alerts.component';
+import { RainfallComponent } from './components/rainfall/rainfall.component';
+import { MetadataComponent } from './components/metadata/metadata.component';
 import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
 import { environment } from '../environments/environment.development';
 
