c7a4a9c15151892622b97aa23dac6f488dbb5342
Dániel Boros <daniel.boros.17@gmail.com>
2023-08-16 18:43:25 +0200

    Add forecast data to the forecast component

--- src/app/components/forecast/forecast.component.ts
+++ src/app/components/forecast/forecast.component.ts
@@ -1,8 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, Signal } from '@angular/core';
+import { WeatherService } from '../../services/weather.service';
+import { ForecastWeather } from '../../types/forecast-weather';
+import { conditionCodes } from '../../utils/condition-codes.util';
 
 @Component({
     selector: 'forecast',
     templateUrl: './forecast.component.html',
     styleUrls: ['./forecast.component.css'],
 })
-export class ForecastComponent {}
+export class ForecastComponent implements OnInit {
+    private conditionsService: WeatherService = inject(WeatherService);
+
+    readonly DAYS_OF_WEEK: string[] = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+    ];
+
+    forecastWeather: Signal<ForecastWeather[]> =
+        this.conditionsService.forecastWeather;
+
+    ngOnInit() {}
+
+    createRoundedTemperature(temp: number): number {
+        return Math.round(temp);
+    }
+
+    getConditionIcon(code: number): string {
+        return conditionCodes[code].day;
+    }
+
+    getDayOfWeek(dateString: string): string {
+        const date: Date = new Date(dateString);
+        const dayIndex: number = date.getDay();
+        const dayOfWeek: string = this.DAYS_OF_WEEK[dayIndex];
+
+        return dayOfWeek.substring(0, 3);
+    }
+}
