17801824054b2bc55b541c8f4f47aceab7d37ee9
Dániel Boros <daniel.boros.17@gmail.com>
2023-08-17 19:15:40 +0200

    Add precipitation data to the rainfall chart

--- src/app/components/rainfall/rainfall.component.ts
+++ src/app/components/rainfall/rainfall.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, effect, inject, OnInit, Signal } from '@angular/core';
 import Chart from 'chart.js/auto';
+import { WeatherService } from '../../services/weather.service';
+import { ForecastHour, ForecastWeather } from '../../types/forecast-weather';
 
 @Component({
     selector: 'rainfall',
@@ -7,16 +9,27 @@ import Chart from 'chart.js/auto';
     styleUrls: ['./rainfall.component.css'],
 })
 export class RainfallComponent implements OnInit {
+    private conditionsService: WeatherService = inject(WeatherService);
+
+    forecastWeather: Signal<ForecastWeather[]> =
+        this.conditionsService.forecastWeather;
+
+    readonly FORECAST_HOUR_COUNT = 12;
+
     chart: Chart | undefined;
+
+    constructor() {
+        effect(() => this.refreshChartData());
+    }
 
     ngOnInit() {
         this.chart = new Chart('canvas', {
             type: 'bar',
             data: {
-                labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+                labels: [],
                 datasets: [
                     {
-                        data: [10, 5, 0, 2, 15, 18, 12, 0, 0, 2],
+                        data: [],
                         label: '',
                         backgroundColor: '#36a2eb',
                         borderRadius: 5,
@@ -32,5 +45,59 @@ export class RainfallComponent implements OnInit {
                 responsive: true,
             },
         });
+    }
+
+    private initChartData(): Map<number, number> | undefined {
+        const precipitationPerHour: Map<number, number> = new Map<
+            number,
+            number
+        >();
+        const currentHour: number = new Date().getHours();
+        const forecastWeather: ForecastWeather[] = this.forecastWeather();
+
+        if (forecastWeather.length === 0) return;
+
+        const currentDayHours: ForecastHour[] = forecastWeather[0].hour;
+
+        // Iterate the hours of the current day and add the precipitation data
+        for (let i = currentHour; i < currentDayHours.length; i++) {
+            const precipitation: number = currentDayHours[i].precipitation ?? 0;
+
+            precipitationPerHour.set(i, precipitation);
+        }
+
+        const elapsedHours: number = currentDayHours.length - currentHour;
+
+        // Skip to next day and add the remaining hours
+        if (elapsedHours < this.FORECAST_HOUR_COUNT) {
+            const remainingHours: number =
+                this.FORECAST_HOUR_COUNT - elapsedHours;
+            const nextDayHours: ForecastHour[] = forecastWeather[1].hour;
+
+            for (let i = 0; i < remainingHours; i++) {
+                const precipitation: number =
+                    nextDayHours[i].precipitation ?? 0;
+
+                precipitationPerHour.set(i, precipitation);
+            }
+        }
+
+        return precipitationPerHour;
+    }
+
+    private refreshChartData(): void {
+        const chartData: Map<number, number> | undefined = this.initChartData();
+
+        if (!chartData || !this.chart) return;
+
+        const chartKeys: string[] = [...chartData.keys()].map((key) =>
+            key.toString()
+        );
+        const chartValues: number[] = [...chartData.values()];
+
+        this.chart.data.labels = chartKeys;
+        this.chart.data.datasets[0].data = chartValues;
+
+        this.chart.update();
     }
 }
